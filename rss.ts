c430005c12a7a4f0cc0394116d499ca5c0169853
shaahid <writetoshaahid@gmail.com>
2025-11-27 01:29:53 +0530

    update: rss.ts

--- rss.ts
+++ rss.ts
@@ -8,7 +8,7 @@ export default async function generateRssFeed() {
 
   const feedOptions = {
     title: `Blogs`,
-    description: "Hey! I am Gxuri, and its my personal blog where I share my learnings, experiences, and thoughts on different topics. I mostly talk about tech, but I don't have any specific niche.  I write about whatever I find interesting. I hope you will find my blogs helpful. Happy reading!",
+    description: "Hey! I am shaahid a 20yo CSE student trying to make cool stuff and documenting it to share with people. Happy reading!",
     id: site_url,
     link: site_url,
     image: `${site_url}/logo.png`,
